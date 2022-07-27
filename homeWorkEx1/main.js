987c7b4378ff530d75aade9b29a9f676bcd88f3d
Homka23 <homka23052005@gmail.com>
2022-07-27 08:23:58 +0200

    Initial commit

---
+++ homeWorkEx1/main.js
@@ -0,0 +1,138 @@
+let numbers = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+];
+console.log(numbers[0]);
+console.log(numbers[1]);
+console.log(numbers[2]);
+console.log(numbers[3]);
+console.log(numbers[4]);
+console.log(numbers[5]);
+console.log(numbers[6]);
+console.log(numbers[7]);
+console.log(numbers[8]);
+console.log(numbers[9]);
+
+
+// let book1 = {
+//     title: 'The Great Gatsby',
+//     pageCount: 234,
+//     genre: 'Action'
+// };
+//
+// let book2 = {
+//     title: 'Invisible Man',
+//     pageCount: 544,
+//     genre: 'Comedy'
+// };
+//
+// let book3 = {
+//     title: 'Beloved',
+//     pageCount: 371,
+//     genre: 'Horror'
+// };
+
+let book1 = {
+    title: 'The Great Gatsby',
+    pageCount: 234,
+    genre: 'Action',
+    autors: [
+        autor1 = {
+            name: 'Suresh Raina',
+            age: 54
+        },
+        autor2 = {
+            name: 'Jhumpa Lahiri',
+            age: 21
+        }
+    ]
+};
+console.log(book1.autors[1]);
+
+
+let book2 = {
+    title: 'Invisible Man',
+    pageCount: 544,
+    genre: 'Comedy'
+};
+
+let book3 = {
+    title: 'Beloved',
+    pageCount: 371,
+    genre: 'Horror',
+    autors: [
+        let = autor1 = {
+            name: 'PM Narendra Modi',
+            age: 34
+        },
+        let = autor2 = {
+            name: 'JK Rowling',
+            age: 87
+        }
+    ]
+};
+
+
+
+let users = [
+    user1 = {
+        name: 'Lucina Victoria',
+        username: 'Victoria34',
+        password: '245839480jcjc'
+    },
+    user2 = {
+        name: ' Aeneas Lara ',
+        username: 'Lara64',
+        password: 'kjdf4553'
+    },
+    user3 = {
+        name: ' Nisus Janus ',
+        username: 'Janus743',
+        password: 'jesallkls56'
+    },
+    user4 = {
+        name: ' Nona Neptuno ',
+        username: 'Neptuno54',
+        password: 'lssmnnde4323'
+    },
+    user5 = {
+        name: ' Ascanius Cardea ',
+        username: 'Cardea72',
+        password: 'jrjqwljel2'
+    },
+    user6 = {
+        name: ' Evander Saturn ',
+        username: 'Saturn4567',
+        password: 'kjhgfd23'
+    },
+    user7 = {
+        name: ' Vênus Camilla ',
+        username: 'Camilla8654',
+        password: 'qwe4rt5y'
+    },
+    user8 = {
+        name: ' Janus Proserpine ',
+        username: 'Proserpine44565',
+        password: 'kjkjejwlljll445'
+    },
+    user9 = {
+        name: ' Nettuno Juventas ',
+        username: 'Juventas456',
+        password: 'mnbvcx34567'
+    },
+    user10 = {
+        name: ' Dido Summanus ',
+        username: 'Summanus876',
+        password: 'trhgjmn9876'
+    }
+];
+
+console.log(users[0]['password']);
+console.log(users[1]['password']);
+console.log(users[2]['password']);
+console.log(users[3]['password']);
+console.log(users[4]['password']);
+console.log(users[5]['password']);
+console.log(users[6]['password']);
+console.log(users[7]['password']);
+console.log(users[8]['password']);
+console.log(users[9]['password']);
